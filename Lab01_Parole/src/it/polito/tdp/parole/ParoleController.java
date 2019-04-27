fa0986aa04580aaa5da1f073390437f630f8d7cd
mastr <mastr@ACER-SWIFT-3>
2019-04-27 16:35:03 +0200

    v.1.2
    completato e ottimizzato

--- Lab01_Parole/src/it/polito/tdp/parole/ParoleController.java
+++ Lab01_Parole/src/it/polito/tdp/parole/ParoleController.java
@@ -32,6 +32,9 @@ public class ParoleController {
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
     
+    @FXML // fx:id="txtPerformance"
+    private TextArea txtPerformance; // Value injected by FXMLLoader
+    
     @FXML // fx:id="btnDelete"
     private Button btnDelete; // Value injected by FXMLLoader
     
@@ -47,15 +50,23 @@ public class ParoleController {
     	
     	String parola = txtParola.getText().toLowerCase().trim();
     	
-    	if(txtParola.getText().length()==0 ) {
+    	if(txtParola.getText().length()==0) {
     		txtResult.appendText("Inserire una parola \n");
     		return;
     		}else {
-    	elenco.addParola(parola);
+    			
+    			double start = System.nanoTime();
+		    	elenco.addParola(parola);
+		    	double stop = System.nanoTime();
+		    	
+		    	for (String p : elenco.getElenco()) {
+					this.txtResult.appendText(p+"\n");
+					}
+		    	
+		    	this.txtPerformance.setText("[INSERT]: "+(stop-start)/(1000*1000) + " secondi");
+
+		    	}
     	
-    	for (String p : elenco.getElenco()) {
-			this.txtResult.appendText(p+"\n");
-		}}
     	this.txtParola.clear();
     }
     
@@ -72,14 +83,19 @@ public class ParoleController {
     		}else {
     	elenco.addParola(parola);
     	
-    	
+		double start = System.nanoTime();
     	boolean isDeleted = elenco.delete(parola);
+    	double stop = System.nanoTime();
     	
+
     	if(isDeleted==true) {
     	
 	    	for (String p : elenco.getElenco()) {
 				this.txtResult.appendText(p+"\n");
-			}}
+			}
+	    	this.txtPerformance.setText("[REMOVE]: "+(stop-start)/(1000*1000) + " secondi");
+    	
+    	}
     	
     	if(isDeleted==false)
 				txtResult.setText("parola non eliminata perchè non presente");
@@ -94,13 +110,18 @@ public class ParoleController {
     	elenco.reset();
     	this.txtResult.clear();
     	this.txtParola.clear();
+    	txtPerformance.clear();
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Parole.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert txtPerformance != null : "fx:id=\"txtPerformance\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert btnDelete != null : "fx:id=\"btnDelete\" was not injected: check your FXML file 'Parole.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Parole.fxml'.";
+
 
         elenco = new Parole() ;
         
